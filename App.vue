a07f9b4c3e7c306e4f272760c041d93dd9103f10
sagars1ngh <143807975+sagars1ngh@users.noreply.github.com>
2023-09-22 11:32:31 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1> <!-- Text Interpolation -->
+    <p v-html="rawHTML"></p> <!-- Raw HTML -->
+    <div :id="dynamicId">{{ dynamicText }}</div> <!-- Attribute Bindings -->
+    <p>{{ 2 + 2 }}</p> <!-- JavaScript expressions inside syntax -->
+
+    <button @click="sayHello">Click me</button> <!-- Event Handling -->
+
+    <p>Reactive Counter: {{ counter }}</p> <!-- Reactivity Fundamentals -->
+    <button @click="incrementCounter">Increment</button>
+
+    <p>Computed Property: {{ computedValue }}</p> <!-- Computed Properties -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello, Vue 3!',
+      rawHTML: '<span style="color: red;">Red Text</span>',
+      dynamicId: 'elementId',
+      dynamicText: 'Dynamic Text',
+      counter: 0,
+    };
+  },
+  computed: {
+    computedValue() {
+      return this.counter * 2;
+    },
+  },
+  methods: {
+    sayHello() {
+      alert('Hello from Vue 3!');
+    },
+    incrementCounter() {
+      this.counter++;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* CSS specific to this component */
+</style>
